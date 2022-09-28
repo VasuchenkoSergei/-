add1ae19523747b075a2b5270f7ba19b4d34772c
Васюченко Сергей <zzbrimsz@gmail.com>
2022-09-28 22:15:14 +0300

    commit

---
+++ Router.php
@@ -0,0 +1,15 @@
+<?php
+/*Необходимо создать в папке core класс Router
+В Router создать метод run() который будет через var_export выдавать
+ свойства класса.
+Необходимо подключить в index.php используя namespace класс Router*/
+namespace core
+class Router 
+{
+    public $a = 5;
+    public function run($a) 
+    {
+        var_export(run);
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'vendor/autoload.php';
+us core\Router;
+$a = new Router();
+?>
